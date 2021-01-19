54f86896c8922eaaaf896d57921c586e9bbd19c6
Flø <florian.boret@data-wax.com>
2021-01-19 22:35:06 +0100

    ajout du type

--- a_preparation_des_donnees.sh
+++ a_preparation_des_donnees.sh
@@ -13,6 +13,7 @@ for val in ${StringArray[@]}; do
    echo $val
    grep -E "D$val|DEP_$val" liens_ign.txt | awk '{ printf("%s,D'$val'\n", $0); }' > dep/D$val.csv
 done
+
 #Fusion des fichiers csv
 cat dep/*csv > liens_par_dep.csv
 #Permet de supprimer les doublons
@@ -20,3 +21,16 @@ sort -u liens_par_dep.csv -o liens_par_dep_clean.csv
 
 #Conserve uniquement les liens avec une extension .7z
 grep -F '.7z' liens_par_dep_clean.csv > liens_par_dep_clean_ext.csv
+
+#création du répertoire type
+mkdir type
+
+#Permet de créer des fichiers par type
+declare -a StringArray=("BDTOPO_3-0_TOUSTHEMES" "PLANIGN__TIF_LAMB93" "ORTHOHR_1-0_RVB-0M20_JP2-E080_LAMB93" "BDFORET_V2")
+for val in ${StringArray[@]}; do
+   echo $val
+   grep -E "$val" liens_par_dep_clean_ext.csv | awk '{ printf("%s,'$val'\n", $0); }' > type/$val.csv
+done
+
+#Fusion des fichiers csv
+cat type/*csv > liens_par_type.csv
