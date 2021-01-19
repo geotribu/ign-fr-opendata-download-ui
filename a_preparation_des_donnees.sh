2be6eb2095bbff8d33f6c14c32e07d21272c36d8
Flø <florian.boret@data-wax.com>
2021-01-19 22:11:33 +0100

    Liens avec une extension

--- a_preparation_des_donnees.sh
+++ a_preparation_des_donnees.sh
@@ -116,3 +116,6 @@ cat dep/*csv > liens_par_dep.csv
 
 #Permet de supprimer les doublons
 sort -u liens_par_dep.csv -o liens_par_dep_clean.csv
+
+#Conserve uniquement les liens avec une extension .7z
+grep -F '.7z' liens_par_dep_clean.csv > liens_par_dep_clean_ext.csv 
