ea56d72d0ec7252e491d4ed1f47f3e1192538957
GeoJulien <dev@ingeoveritas.com>
2021-01-24 20:08:25 +0100

    improve sub scripts

--- scripts/1_scraper.sh
+++ scripts/1_scraper.sh
@@ -1,6 +1,12 @@
 # Première étape : scraping de la page de l'IGN
-[ -d _temp/1_scraping_liens ] || mkdir -p _temp/1_scraping_liens
+
+# Arguments
+SOURCE_URL=$1
+OUTPUT_FILE=$2
+
+# si le dossier n'existe pas, on le crée
+[ -d $OUTPUT_FILE ] || mkdir -p $OUTPUT_FILE
 
 # télécharge la page HTML (stream), extrait les liens et le stockent dans un fichier texte
-curl https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html |\
-    grep -oE '\b(https?|ftp|file)://[-A-Za-z0-9+&@# /%?=~_|!:,.;]*[-A-Za-z0-9+&@# /%=~_|]'  > _temp/1_liens/liens_ign.txt
+curl  |\
+    grep -oE '\b(https?|ftp|file)://[-A-Za-z0-9+&@# /%?=~_|!:,.;]*[-A-Za-z0-9+&@# /%=~_|]'  > $OUTPUT_FILE
--- scripts/2_departements.sh
+++ scripts/2_departements.sh
@@ -1,14 +1,20 @@
-# Création du répertoire 2_dep
-[ -d _temp/2_departements ] || mkdir -p _temp/2_departements
+# Deuxième étape : extraction des liens
+
+# Arguments
+INPUT_FOLDER=$1
+OUTPUT_FOLDER=$2
+DPTS_ARRAY=$3
+
+# si le dossier n'existe pas, on le crée
+[ -d $OUTPUT_FOLDER ] || mkdir -p $OUTPUT_FOLDER
 
 # Permet de créer des fichiers par département
-declare -a StringArray=("001" "002" "003" "004" "005" "006" "007" "008" "009" "010" "011" "012" "013" "014" "015" "016" "017" "018" "019" "02A" "02B" "002A" "002B" "021" "022" "023" "024" "025" "026" "027" "028" "029" "030" "031" "032" "033" "034" "035" "036" "037" "038" "039" "040"  "041" "042" "043" "044" "045" "046" "047" "048" "049" "050" "051" "052" "053" "054" "055" "056" "057" "058" "059" "060" "061" "062" "063" "064" "065" "066" "067" "068" "069" "070" "071" "072" "073" "074" "075" "076" "077" "078" "079" "080" "081" "082" "083" "084" "085" "086"  "087"  "088"  "089" "090" "091" "092" "093" "094" "095" "971" "972" "973" "974" "975" "976")
-for val in ${StringArray[@]}; do
+for val in ${DPTS_ARRAY[@]}; do
    echo $val
    # Erreur sur la corse
    if [ "$val" = "002A" ] || [ "$val" = "002B" ]; then
-        grep -E "D$val|DEP_$val" _temp/1_scraping_liens/liens_ign.txt | awk '{ printf("%s,D'${val: -3}'\n", $0); }' >> '2_dep/D'${val: -3}'.csv'
+        grep -E "D$val|DEP_$val" $TEMP_FOLDER/1_scraping_liens/liens_ign.txt | awk '{ printf("%s,D'${val: -3}'\n", $0); }' >> '2_dep/D'${val: -3}'.csv'
       else
-        grep -E "D$val|DEP_$val" _temp/1_scraping_liens/liens_ign.txt | awk '{ printf("%s,D'$val'\n", $0); }' > _temp/2_departements/D$val.csv
+        grep -E "D$val|DEP_$val" $TEMP_FOLDER/1_scraping_liens/liens_ign.txt | awk '{ printf("%s,D'$val'\n", $0); }' > $OUTPUT_FOLDER/D$val.csv
       fi
 done
