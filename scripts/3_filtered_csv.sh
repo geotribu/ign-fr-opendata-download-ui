27ca6e36db9a30b5b29c809f65b32c0f8e322b05
Flø <florian.boret@laposte.net>
2021-01-24 18:38:03 +0100

    Revert "[WIP] Refacto"

--- scripts/3_filtered_csv.sh
+++
@@ -1,5 +0,0 @@
-# Création du répertoire
-[ -d _temp/3_filtered_csv ] || mkdir -p _temp/3_filtered_csv
-
-# Permet de supprimer les doublons et conserve uniquement les liens avec une extension .7z
-cat _temp/2_departements/*csv | sort -u | grep -F '.7z' > _temp/3_filtered_csv/3_liens_par_dep_clean_ext.csv
