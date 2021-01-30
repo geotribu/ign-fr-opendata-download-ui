2c0f414103a8d0343ea26a081b9238894bd81281
Flø <florian.boret@data-wax.com>
2021-01-30 23:51:47 +0100

    s

--- scripts/3_filtered_csv.sh
+++ scripts/3_filtered_csv.sh
@@ -15,4 +15,4 @@ OUTPUT_DIR=$1
 # Permet de supprimer les doublons et conserve uniquement les liens avec une extension .7z
 cat _temp/2_departements/*csv | sort -u | grep -F '.7z' > "$OUTPUT_DIR/3_liens_par_dep_clean_ext.csv"
 cat _temp/2_france/*csv | sort -u | grep -F '.7z' > "$OUTPUT_DIR/3_liens_france_clean_ext.csv"
-cat _temp/2_regions/*csv | sort -u | grep -F '.7z' > "$OUTPUT_DIR/3_liens_par_region_clean_ext.csv"
+cat _temp/2_regions/*csv | sort -u | grep -F '.7z' > "$OUTPUT_DIR/3_liens_par_regions_clean_ext.csv"
