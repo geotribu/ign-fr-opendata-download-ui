8fa899bf7f5fadd555d83941c56da825d287b02e
Flø <florian.boret@data-wax.com>
2021-01-30 19:48:48 +0100

    Update 4_csv_type.sh

--- scripts/4_csv_type.sh
+++ scripts/4_csv_type.sh
@@ -1,16 +1,31 @@
-# Création du répertoire
-[ -d _temp/4_csv_type ] || mkdir -p _temp/4_csv_type
+# quatrième étape : Permet de créer des fichiers par catégrorie
 
-# Permet de créer des fichiers par type
-declare -a StringArray=("BDTOPO" "PLANIGN" "ORTHOHR" "BDFORET" "DNSB-HAIES" "PARCELLAIRE_EXPRESS" "BDPARCELLAIRE" "RGEALTI" "BDORTHO")
-for val in ${StringArray[@]}; do
+# Le petit manuel
+if [ "$1" == "-h" ] ; then
+    echo "Usage: ./`basename $0` departement _temp/3_filtered_csv/3_liens_par_dep_clean_ext.csv _temp/4_csv_type \"BDTOPO,PLANIGN,ORTHOHR,BDFORET,DNSB-HAIES,PARCELLAIRE_EXPRESS,BDPARCELLAIRE,RGEALTI,BDORTHO\""
+    echo "Show this help: ./`basename $0` -h"
+    exit 0
+fi
+
+# Arguments
+TYPE=$1
+SOURCE_FILE=$2
+OUTPUT_DIR=$3
+IFS="," read -a ARRAY_DEPARTEMENTS <<< $4
+
+# Création du répertoire parent
+[ -d $OUTPUT_DIR ] || mkdir -p $OUTPUT_DIR
+
+
+# Permet de créer des fichiers par catégorie
+for val in ${ARRAY_DEPARTEMENTS[@]}; do
    echo $val
    value="$(tr [A-Z] [a-z] <<< "${val//-/_}")"
-   grep -E "$val" _temp/3_filtered_csv/3_liens_par_dep_clean_ext.csv | awk '{ printf("%s,'$val'\n", $0); }' | sort -s -k2,2 -t, > _temp/4_csv_type/$value.csv
-   awk -F',' -v OFS=',' '{if(a!=$2){printf (a!="")?"\n"$2:$2;a=$2} printf "%s%s",OFS,$1}END{}' _temp/4_csv_type/$value.csv > _temp/4_csv_type/$value'_transposition.csv' # Transposition des tableaux
+   grep -E "$val" $SOURCE_FILE | awk '{ printf("%s,'$val'\n", $0); }' | sort -s -k2,2 -t, > $OUTPUT_DIR/$value'_'$TYPE'.csv'
+   awk -F',' -v OFS=',' '{if(a!=$2){printf (a!="")?"\n"$2:$2;a=$2} printf "%s%s",OFS,$1}END{}' $OUTPUT_DIR/$value'_'$TYPE'.csv' > $OUTPUT_DIR/$value'_'$TYPE'_transposition.csv' # Transposition des tableaux
 
 
-   count_column=$(head -2 _temp/4_csv_type/$value'_transposition.csv' |tail -1 |tr '\,' '\n' |wc -l)
+   count_column=$(head -2 $OUTPUT_DIR/$value'_'$TYPE'_transposition.csv' | tail -1 |tr '\,' '\n' |wc -l)
    column=""
    column_sql=""
    for ((i=1; i<=($count_column); i++)) {
