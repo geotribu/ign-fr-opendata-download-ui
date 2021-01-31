486e1272592893f03474ac50cee739cc8c99eb17
GeoJulien <dev@ingeoveritas.com>
2021-01-31 20:02:45 +0100

    Fix lint warnings

--- scripts/1_scraper.sh
+++ scripts/1_scraper.sh
@@ -2,9 +2,9 @@
 # Première étape : scraping de la page de l'IGN
 
 # Le petit manuel
-if [ "$1" == "-h" ] ; then
-    echo "Usage: ./`basename $0` https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html liens_ign.txt"
-    echo "Show this help: ./`basename $0` -h"
+if [ "$1" = "-h" ] ; then
+    echo "Usage: ./$(basename $0) https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html liens_ign.txt"
+    echo "Show this help: ./$(basename $0) -h"
     exit 0
 fi
 
@@ -13,12 +13,12 @@ SOURCE_URL=$1
 OUTPUT_FILE=$2
 
 # si le dossier parent n'existe pas, on le crée    
-[ -d $OUTPUT_FILE ] || mkdir -p "$(dirname "$OUTPUT_FILE")"
+[ -d "$OUTPUT_FILE" ] || mkdir -p "$(dirname "$OUTPUT_FILE")"
 # on supprime d'abord les anciens fichiers
-rm $OUTPUT_FILE
+rm "$OUTPUT_FILE"
 
 echo "Téléchargement de la page : $SOURCE_URL"
 
 # télécharge la page HTML (stream), extrait les liens et le stockent dans un fichier texte
-curl $SOURCE_URL | \
-    grep -oE '\b(https?|ftp|file)://[-A-Za-z0-9+&@# /%?=~_|!:,.;]*[-A-Za-z0-9+&@# /%=~_|]' > $OUTPUT_FILE
+curl "$SOURCE_URL" | \
+    grep -oE '\b(https?|ftp|file)://[-A-Za-z0-9+&@# /%?=~_|!:,.;]*[-A-Za-z0-9+&@# /%=~_|]' > "$OUTPUT_FILE"
