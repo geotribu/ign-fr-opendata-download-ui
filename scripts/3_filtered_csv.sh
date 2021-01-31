0450e0535dac22a68462524d47215797e5fb20ce
GeoJulien <dev@ingeoveritas.com>
2021-01-31 20:53:11 +0100

    Ajout étape 3

--- scripts/3_filtered_csv.sh
+++ scripts/3_filtered_csv.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Troisième étape : Permet de supprimer les doublons et conserve uniquement les liens avec une extension .7z
 
 # Le petit manuel
@@ -8,12 +8,13 @@ if [ "$1" == "-h" ] ; then
     exit 0
 fi
 
-OUTPUT_DIR=$1
+INPUT_DIR=$1
+OUTPUT_DIR=$2
 
 # Création du répertoire parent
-[ -d $OUTPUT_DIR ] || mkdir -p $OUTPUT_DIR
+[ -d "$OUTPUT_DIR" ] || mkdir -p "$OUTPUT_DIR"
 
 # Permet de supprimer les doublons et conserve uniquement les liens avec une extension .7z
-cat _temp/2_departements/*csv | sort -u | grep -F '.7z' > "$OUTPUT_DIR/3_liens_par_dep_clean_ext.csv"
-cat _temp/2_france/*csv | sort -u | grep -F '.7z' > "$OUTPUT_DIR/3_liens_france_clean_ext.csv"
-cat _temp/2_regions/*csv | sort -u | grep -F '.7z' > "$OUTPUT_DIR/3_liens_par_regions_clean_ext.csv"
+cat "$INPUT_DIR"/2_departements/*csv | sort -u | grep -F '.7z' > "$OUTPUT_DIR/3_liens_par_dep_clean_ext.csv"
+cat "$INPUT_DIR"/2_france/*csv | sort -u | grep -F '.7z' > "$OUTPUT_DIR/3_liens_france_clean_ext.csv"
+cat "$INPUT_DIR"/2_regions/*csv | sort -u | grep -F '.7z' > "$OUTPUT_DIR/3_liens_par_regions_clean_ext.csv"
