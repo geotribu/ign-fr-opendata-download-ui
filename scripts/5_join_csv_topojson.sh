0e9f4ed7ff0473cadee2653f96dedc27d50a3c65
GeoJulien <dev@ingeoveritas.com>
2021-02-07 21:10:07 +0100

    echoing

--- scripts/5_join_csv_topojson.sh
+++ scripts/5_join_csv_topojson.sh
@@ -27,28 +27,30 @@ fi
 [ -d $OUTPUT_DIR ] || mkdir -p $OUTPUT_DIR
 
 
-echo "coucou $SCALE $STYPE $SOURCE_DIR $TEMPLATES_DIR $OUTPUT_DIR"
+echo "$SCALE $STYPE $SOURCE_DIR $TEMPLATES_DIR $OUTPUT_DIR"
 
 for val in ${ARRAY[@]}; do
    echo $val
    value="$(tr [A-Z] [a-z] <<< "${val//-/_}")"
 
     count_column=$(awk -F'\,' '{print NF}' '_temp/4_csv_type/'$value'_'$SCALE'_transposition.csv' | sort -nu | tail -n 1)
+    echo $count_column
 
     column_list=""
     for ((i=1; i<=($count_column); i++)) {
         column_list+="field"$i" "
     }
+    echo $column_list
 
     cp "$TEMPLATES_DIR/$SCALE-avec-outre-mer.json" "$OUTPUT_DIR/$SCALE_$value.json"
 
-    (cat $SOURCE_DIR/$value'_'$SCALE'_transposition.csv'; echo) | while IFS=, read -r $column_list; do
-        column_json=""
-        for ((i=1; i<=($count_column); i++)) {
-              test="field$i"
-              column_json+=',"lien'$i'":"'${!test}'"'
-        }
-        sed -i 's~,\"DEP\":\"'$field1'\"~,\"DEP\":\"'$field1'\"'${column_json}'~g' $OUTPUT_DIR/departement_$value.json
-    done <  _temp/4_csv_type/$value'_transposition.csv'
+    # (cat $SOURCE_DIR/$value'_'$SCALE'_transposition.csv'; echo) | while IFS=, read -r $column_list; do
+    #     column_json=""
+    #     for ((i=1; i<=($count_column); i++)) {
+    #           test="field$i"
+    #           column_json+=',"lien'$i'":"'${!test}'"'
+    #     }
+    #     sed -i 's~,\"DEP\":\"'$field1'\"~,\"DEP\":\"'$field1'\"'${column_json}'~g' $OUTPUT_DIR/departement_$value.json
+    # done <  _temp/4_csv_type/$value'_transposition.csv'
 
 done
