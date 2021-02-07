bd8261f9bc8c6b5fd629d7573ffe65fcf7921404
GeoJulien <dev@ingeoveritas.com>
2021-02-07 20:33:33 +0100

    Start to bump the fifth script

--- scripts/5_join_csv_topojson.sh
+++ scripts/5_join_csv_topojson.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # cinquième étape : Jointure du csv avec le fichier topojson
 
 # Le petit manuel
@@ -9,16 +9,17 @@ if [ "$1" == "-h" ] ; then
 fi
 
 # Arguments
-TYPE=$1
-SOURCE_FILE=$2
-OUTPUT_DIR=$3
-IFS="," read -a ARRAY <<< $4
+SCALE=$1
+SOURCE_DIR=$2
+TEMPLATES_DIR=$3
+OUTPUT_DIR=$4
+IFS="," read -a ARRAY <<< $5
 
-if [ "$TYPE" == "departements" ] ; then
+if [ "$SCALE" == "departements" ] ; then
 STYPE="DEP"
-elif [ "$TYPE" == "regions" ] ; then
+elif [ "$SCALE" == "regions" ] ; then
 STYPE="REG"
-elif [ "$TYPE" == "france" ] ; then
+elif [ "$SCALE" == "france" ] ; then
 STYPE="FR"
 fi
 
@@ -26,20 +27,22 @@ fi
 [ -d $OUTPUT_DIR ] || mkdir -p $OUTPUT_DIR
 
 
+echo "coucou $SCALE $STYPE $SOURCE_DIR $TEMPLATES_DIR $OUTPUT_DIR"
+
 for val in ${ARRAY[@]}; do
    echo $val
    value="$(tr [A-Z] [a-z] <<< "${val//-/_}")"
 
-    count_column=$(awk -F'\,' '{print NF}' _temp/4_csv_type/$value'_'$TYPE'_transposition.csv' | sort -nu | tail -n 1)
+    count_column=$(awk -F'\,' '{print NF}' _temp/4_csv_type/$value'_'$SCALE'_transposition.csv' | sort -nu | tail -n 1)
 
     column_list=""
     for ((i=1; i<=($count_column); i++)) {
         column_list+="field"$i" "
     }
 
-    cp templates/$TYPE'-avec-outre-mer.json' $OUTPUT_DIR/$TYPE"_"$value'.json'
+    cp $TEMPLATES_DIR/$SCALE'-avec-outre-mer.json' $OUTPUT_DIR/$SCALE"_"$value'.json'
 
-    (cat $SOURCE_FILE/$value'_'$TYPE'_transposition.csv'; echo) | while IFS=, read -r $column_list; do
+    (cat $SOURCE_DIR/$value'_'$SCALE'_transposition.csv'; echo) | while IFS=, read -r $column_list; do
         column_json=""
         for ((i=1; i<=($count_column); i++)) {
               test="field$i"
