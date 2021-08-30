40e24cd2c350b432ade125556f268020c386da21
Flø <florian.boret@data-wax.com>
2021-08-30 11:09:15 +0200

    URL Geoservices

--- scripts/1_scraper.sh
+++ scripts/1_scraper.sh
@@ -3,7 +3,7 @@
 
 # Le petit manuel
 if [ "$1" = "-h" ] ; then
-    echo "Usage: ./$(basename $0) https://geoservices.ign.fr/documentation/diffusion/telechargement-donnees-libres.html liens_ign.txt"
+    echo "Usage: ./$(basename $0) https://geoservices.ign.fr/planign,https://geoservices.ign.fr/bdortho,https://geoservices.ign.fr/bdtopo,https://geoservices.ign.fr/bdforet,https://geoservices.ign.fr/rgealti,https://geoservices.ign.fr/parcellaire-express-pci,https://geoservices.ign.fr/rpg,https://geoservices.ign.fr/scan50,https://geoservices.ign.fr/scanhisto,https://geoservices.ign.fr/bdhaie,https://geoservices.ign.fr/ocsge,https://geoservices.ign.fr/bdalti,https://geoservices.ign.fr/bdcarto,https://geoservices.ign.fr/bdparcellaire,https://geoservices.ign.fr/route500,https://geoservices.ign.fr/scan1000 liens_ign.txt"
     echo "Show this help: ./$(basename $0) -h"
     exit 0
 fi
@@ -17,8 +17,11 @@ OUTPUT_FILE=$2
 # on supprime d'abord les anciens fichiers
 rm "$OUTPUT_FILE"
 
-echo "Téléchargement de la page : $SOURCE_URL"
-
+IFS="," read -a ARRAY_URL <<< $SOURCE_URL
+for val in ${ARRAY_URL[@]}; do
+echo "Téléchargement de la page : $val"
 # télécharge la page HTML (stream), extrait les liens et le stockent dans un fichier texte
-curl "$SOURCE_URL" | \
-    grep -oE '\b(https?|ftp|file)://[-A-Za-z0-9+&@# /%?=~_|!:,.;]*[-A-Za-z0-9+&@# /%=~_|]' > "$OUTPUT_FILE"
+curl "$val" | \
+    grep -oE '\b(https?|ftp|file)://[-A-Za-z0-9+&@# /%?=~_|!:,.;]*[-A-Za-z0-9+&@# /%=~_|]' >> "$OUTPUT_FILE"
+
+done
